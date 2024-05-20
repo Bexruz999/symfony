dd303d8de7338d1e563a1f249f6f2465971eeb81
Bexruz999 <bexruzfatullayev999@gmail.com>
2024-05-20 23:40:34 +0500

    package.json

--- src/Controller/Admin/RecipeController.php
+++ src/Controller/Admin/RecipeController.php
@@ -113,4 +113,5 @@ class RecipeController extends AbstractController
         $this->addFlash('success', 'Recipe deleted.');
         return $this->redirectToRoute('admin.recipe.index');
     }
+    //test
 }
